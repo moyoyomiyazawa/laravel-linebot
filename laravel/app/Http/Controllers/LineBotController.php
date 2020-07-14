35cdfb3ff256cf4d5bc226286b1539cfc9e16660
Yuya Kasai <moyoyomiyazawa@gmail.com>
2020-07-14 21:35:53 +0900

    オウム返しbot実装

--- laravel/app/Http/Controllers/LineBotController.php
+++ laravel/app/Http/Controllers/LineBotController.php
@@ -3,8 +3,47 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use LINE\LINEBot\Event\MessageEvent\TextMessage;
 
 class LineBotController extends Controller
 {
-    //
+
+    public function index()
+    {
+        return view('linebot.index');
+    }
+
+    public function parrot(Request $request)
+    {
+        // ログファイルに情報を出力
+        Log::debug($request->header());
+        Log::debug($request->input());
+
+        $httpClient = new CurlHTTPClient(config('constants.line_access_token'));
+        $lineBot = new LINEBot($httpClient, ['channelSecret' => config('constants.line_channel_secret')]);
+
+        // 署名の検証
+        $signature = $request->header('x-line-signature');
+        if (!$lineBot->validateSignature($request->getContent(), $signature)) {
+            abort(400, 'Invalid signature');
+        }
+
+        $events = $lineBot->parseEventRequest($request->getContent(), $signature);
+
+        Log::debug($events);
+
+        foreach ($events as $event) {
+            if (!($event instanceof TextMessage)) {
+                Log::debug('Non text message has come');
+                continue;
+            }
+
+            $replyToken = $event->getReplyToken();
+            $replyText = $event->getText();
+            $lineBot->replyText($replyToken, $replyText);
+        }
+    }
 }
---
+++ laravel/config/constants.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+
+    'line_access_token' => env('LINE_ACCESS_TOKEN'),
+    'line_channel_secret' => env('LINE_CHANNEL_SECRET'),
+
+];
